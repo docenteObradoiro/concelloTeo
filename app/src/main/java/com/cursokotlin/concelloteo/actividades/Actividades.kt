25cabba49c4a1b0b2fd9978f895ea4ba3949fe48
antiacasvaz <antiaobradoiro@gmail.com>
2022-05-23 12:04:51 +0200

    Arreglo mapas entroido

--- app/src/main/java/com/cursokotlin/concelloteo/actividades/Actividades.kt
+++ app/src/main/java/com/cursokotlin/concelloteo/actividades/Actividades.kt
@@ -13,26 +13,16 @@ class actividades : AppCompatActivity() {
         setContentView(R.layout.activity_actividades)
 
 
-
-       val boton5 = findViewById<ImageButton>(R.id.boton5)
         val boton6=findViewById<ImageButton>(R.id.boton6)
         val boton7=findViewById<ImageButton>(R.id.boton7)
-        val boton8=findViewById<ImageButton>(R.id.boton8)
 
-        boton5.setOnClickListener {
-            val acceder = Intent(this, festasGastronomicas::class.java)
-            startActivity(acceder)
-        }
+
         boton6.setOnClickListener {
             val acceder = Intent(this, entroido::class.java)
             startActivity(acceder)
         }
         boton7.setOnClickListener {
             val acceder = Intent(this, batalla::class.java)
-            startActivity(acceder)
-        }
-        boton8.setOnClickListener {
-            val acceder = Intent(this, nadal::class.java)
             startActivity(acceder)
         }
 
