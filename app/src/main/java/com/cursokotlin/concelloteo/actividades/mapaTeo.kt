9f8fa46d91d063fce682575b1161d10efe1e3502
antiacasvaz <antiaobradoiro@gmail.com>
2022-05-23 12:06:33 +0200

    Arreglo mapas entroido

---
+++ app/src/main/java/com/cursokotlin/concelloteo/actividades/mapaTeo.kt
@@ -0,0 +1,43 @@
+package com.cursokotlin.concelloteo.actividades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cursokotlin.concelloteo.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class mapaTeo : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mapa_teo)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map=googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        val coordinates= LatLng(42.7741010098759,-8.52764312166335)
+        val marker = MarkerOptions().position(coordinates).title("Campo da festa, Teo")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates,18f),
+            4000,
+            null
+        )
+    }
+}
