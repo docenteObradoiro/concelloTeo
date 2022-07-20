7475a82a6f7d66a9a897d7682e1f155caa4a326a
Alex <alexramosclase@gmail.com>
2022-07-20 12:54:39 +0200

    modificaciones de strings, botones agregados y más

--- app/src/main/java/com/cursokotlin/concelloteo/comer/ComerVegana.kt
+++ app/src/main/java/com/cursokotlin/concelloteo/comer/ComerVegana.kt
@@ -1,9 +1,11 @@
 package com.cursokotlin.concelloteo.comer
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
 import com.cursokotlin.concelloteo.MainActivity
 import com.cursokotlin.concelloteo.Menu
 import com.cursokotlin.concelloteo.R
@@ -24,6 +26,38 @@ class ComerVegana : AppCompatActivity() {
             val acceder = Intent(this, Menu::class.java)
             startActivity(acceder)
         }
+
+
+        val direccionFogarDoSantiso = findViewById<TextView>(R.id.direccionFogarDoSantiso)
+        direccionFogarDoSantiso.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://goo.gl/maps/6fCAxx7cdZMccZ9CA")))
+        }
+
+        val telefonoFogarDoSantiso = findViewById<TextView>(R.id.telefonoFogarDoSantiso)
+        telefonoFogarDoSantiso.setOnClickListener {
+            val phone = "tel: + 34 981805948";
+            val intent = Intent (Intent.ACTION_DIAL);
+            intent.setData(Uri.parse(phone));
+            startActivity(intent);
+        }
+
+        val emailFogarDoSantiso = findViewById<TextView>(R.id.emailFogarDoSantiso)
+        emailFogarDoSantiso.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            val recipients = arrayOf("fogar@fogardosantiso.es")
+            intent.putExtra(Intent.EXTRA_EMAIL, recipients)
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Asunto:")
+            intent.putExtra(Intent.EXTRA_TEXT, "")
+            intent.type = "text/html"
+            intent.setPackage("com.google.android.gm")
+            startActivity(Intent.createChooser(intent, "Send mail"))
+        }
+
+        val webFogarDoSantiso = findViewById<TextView>(R.id.webFogarDoSantiso)
+        webFogarDoSantiso.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.fogardosantiso.es")))
+        }
+
 
     }
 }
